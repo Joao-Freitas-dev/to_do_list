de0babffb8dfeb7fbcca6630776e3ca8db7b2a28
joao-freitas-dev <joao.freitasraf@gmail.com>
2024-03-27 12:37:16 -0300

    initial config resolver

--- src/resolvers/toDoList-resolver.ts
+++ src/resolvers/toDoList-resolver.ts
@@ -1,6 +1,12 @@
-import { Resolver } from "type-graphql";
+import { Query, Resolver } from "type-graphql";
 
 //chamando resolver
 @Resolver()
 //Declarando o resolver (ele é como qualquer outra classe)
-export class ToDoListResolver {}
+export class ToDoListResolver {
+    //Para transformar esse código em query vamos utilizar o @Query e precisamos definir o type
+    @Query(() => String)
+    async helloWorld() {
+        return "Hello World";
+    }
+}
