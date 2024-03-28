21f6878c41d0380c0d5f9f67222e5bff7319f573
joao-freitas-dev <joao.freitasraf@gmail.com>
2024-03-28 14:11:07 -0300

    Create resolver query/mutation toDoList

--- src/resolvers/toDoList-resolver.ts
+++ src/resolvers/toDoList-resolver.ts
@@ -1,12 +1,30 @@
-import { Query, Resolver } from "type-graphql";
+import { Arg, Mutation, Resolver, Query } from "type-graphql";
+import { CreateToDoList } from "../dtos/inputs/create-toDoList-input";
+import { ToDoListModel } from "../dtos/models/toDoList-model";
+import { v4 as uuidv4 } from "uuid";
 
 //chamando resolver
 @Resolver()
 //Declarando o resolver (ele é como qualquer outra classe)
 export class ToDoListResolver {
     //Para transformar esse código em query vamos utilizar o @Query e precisamos definir o type
-    @Query(() => String)
-    async helloWorld() {
-        return "Hello World";
+    @Query(() => [ToDoListModel])
+    async allToDoList() {
+        return [{ createdAt: new Date() }];
+    }
+
+    //por se tratar de uma mutation, colocaremos @mutation e o retorno (type) estamos utilizando boolean por enquanto para poder usar o true
+    @Mutation(() => ToDoListModel)
+    //criando mutation to do list
+    async createToDo(@Arg("data") data: CreateToDoList) {
+        const createToDoList = {
+            title: data.title,
+            description: data.description,
+            createdAt: data.createdAt,
+            status: "pending",
+            Id: uuidv4(),
+        };
+
+        return createToDoList;
     }
 }
