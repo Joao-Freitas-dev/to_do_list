274283ff1e6f6dd9db2296d5603a7c8162f2c18f
joao-freitas-dev <joao.freitasraf@gmail.com>
2024-03-29 00:14:03 -0300

    config initial dotenv and dynamodb access server

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,41 @@
 import "reflect-metadata";
-
 import path from "node:path";
-
 import { ApolloServer } from "apollo-server";
 import { buildSchema } from "type-graphql";
 import { ToDoListResolver } from "./resolvers/toDoList-resolver";
+import AWS from "aws-sdk";
+import dotenv from "dotenv";
 
-// A função 'main' é declarada como 'async', o que permite o uso de 'await' dentro dela.
+// Carrega as variáveis de ambiente do arquivo .env
+dotenv.config();
+
+// Configuração do DynamoDB com base nas variáveis de ambiente
+const dynamoDBConfig = {
+    region: process.env.AWS_REGION,
+    endpoint: process.env.AWS_ENDPOINT,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+};
+
+// Configura o SDK da AWS com as informações do DynamoDB local
+AWS.config.update(dynamoDBConfig);
+
+// Crie uma instância do DocumentClient do DynamoDB
+const docClient = new AWS.DynamoDB.DocumentClient();
+
 async function main() {
-    // 'buildSchema' é uma função assíncrona que constrói um esquema GraphQL a partir dos resolvers fornecidos.
-    // 'await' é usado para esperar a promessa ser resolvida antes de atribuir o resultado à variável 'schema'.
+    // Constrói o esquema GraphQL
     const schema = await buildSchema({
         resolvers: [ToDoListResolver],
         emitSchemaFile: path.resolve(__dirname, "schema.gql"),
     });
 
-    // 'ApolloServer' é uma classe que encapsula a configuração de um servidor GraphQL.
-    // O esquema é passado para o construtor do 'ApolloServer'.
+    // Inicia o servidor Apollo GraphQL
     const server = new ApolloServer({ schema });
 
-    // 'server.listen' inicia o servidor e retorna uma promessa que resolve para um objeto com a URL do servidor.
-    // 'await' é usado para esperar a promessa ser resolvida antes de atribuir o resultado à variável 'url'.
     const { url } = await server.listen();
-
-    // A URL do servidor é registrada no console.
-    console.log(`Servidor HTTP rodando em ${url}`);
+    console.log(`Servidor GraphQL rodando em ${url}`);
 }
 
-// A função 'main' é chamada para iniciar o servidor.
+// Chama a função 'main' para iniciar o servidor
 main();
